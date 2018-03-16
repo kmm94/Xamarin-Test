02a3b4e172910e207f6184a98a0cf2928dd109c3
karim møller <karimkuku@gmail.com>
2018-03-16 10:54:28 +0100

    First Blood

---
+++ HWCP/HWCP/HWCP/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HWCP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Page1());
+            Console.WriteLine("FUCK ME");
+        }
+    }
+}
---
+++ HWCP/HWCP/HWCP/WSConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocket4Net;
+
+
+namespace HWCP
+{
+    class WSConnection
+    {
+
+        WebSocket ws;
+
+        public void connect()
+        {
+            Console.WriteLine("before connect");
+            ws = new WebSocket("ws://kikuku.tk:8000/");
+            ws.Opened += new EventHandler(websocket_Opened);
+        }
+
+  
+
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connectted");
+            ws.Send("hello");
+        }
+    }
+}
